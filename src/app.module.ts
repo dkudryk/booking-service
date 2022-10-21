8d12467fa9b3ec5fd36dcc25466dc063e7973903
Dmytro Kudryk <dmytro.kudryk@gmail.com>
2022-10-21 20:37:46 +0300

    ➕ add typeorm + mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_CONNECTION_STRING,
+      database: process.env.MONGODB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      ssl: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
